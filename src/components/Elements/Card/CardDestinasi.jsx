9c41e4a49db22870af585f263aba6e7104e59328
almiramaharani <almira.eka@ui.ac.id>
2023-11-24 21:51:55 +0700

    036 - fix pathname

--- src/components/Elements/Card/CardDestinasi.jsx
+++ src/components/Elements/Card/CardDestinasi.jsx
@@ -1,4 +1,4 @@
-import { LocationIcon } from '@/components/Icons/LocationIcon';
+import { IconLocation } from '@/components/Icons';
 
 export const CardDestinasi = ({image, destination, location, description }) => {
     return (
@@ -6,7 +6,7 @@ export const CardDestinasi = ({image, destination, location, description }) => {
             <img src={image} alt="Destinasi Populer"/>
             <div className="text-left">
                 <div className="flex flex-row mb-4">
-                    <LocationIcon className={'mr-2 location-icon'}/>
+                    <IconLocation className={'mr-2 location-icon'}/>
                     <p className="font-sans text-sm font-normal text-greyDestimate-100">{location}</p>
                 </div>
                 <h3 className='mb-2 font-sans text-xl font-bold text-primary-100'>{destination}</h3>
--- src/components/Icons/IconCompass.jsx
+++ src/components/Icons/IconCompass.jsx
@@ -1,4 +1,4 @@
-export const CompassIcon = () => {
+export const IconCompass = () => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" height="70" viewBox="0 0 24 24">
       <path
